4c47139e3cab1f2941382546edd9e060be2e0dd5
Marwan01 <marwanest@live.fr>
2020-01-07 09:34:13 -0600

    Updates

---
+++ precache-manifest.77bdd702b6c7e01530107914f960a03b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24bea6c8078802fa5d9f6072f7695341",
+    "url": "/mars-dunkin-tweets/index.html"
+  },
+  {
+    "revision": "98235f7d9d248842a235",
+    "url": "/mars-dunkin-tweets/static/css/main.11b2243a.chunk.css"
+  },
+  {
+    "revision": "13b8bc6aa0f6f1e6fe0f",
+    "url": "/mars-dunkin-tweets/static/js/2.9f354e95.chunk.js"
+  },
+  {
+    "revision": "98235f7d9d248842a235",
+    "url": "/mars-dunkin-tweets/static/js/main.56cb4a28.chunk.js"
+  },
+  {
+    "revision": "f9594222e0667e0cc00a",
+    "url": "/mars-dunkin-tweets/static/js/runtime-main.4f9887f5.js"
+  },
+  {
+    "revision": "b49f214ea882fa2d97076112661ce974",
+    "url": "/mars-dunkin-tweets/static/media/dankgif.b49f214e.gif"
+  }
+]);
